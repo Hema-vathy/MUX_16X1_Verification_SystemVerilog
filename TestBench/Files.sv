1c3643a69368679a8b3fef345644caad89374e39
Hema-vathy <158808035+Hema-vathy@users.noreply.github.com>
2025-10-31 19:10:56 +0530

    Add Files block

---
+++ TestBench/Files.sv
@@ -0,0 +1,9 @@
+//files block
+package file_pkg;
+`include "clock.sv"
+`include "reset.sv"
+`include "stimuli.sv"
+`include "rst_test.sv"
+`include "comparison_test.sv"
+`include "run_test.sv"
+endpackage
